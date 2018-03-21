91fd28d122415e6175db5cc2bf02b8b84ffb3493
suming <suming@bih.com>
2018-03-21 12:25:11 +0800

    7th

--- tasks/stktask.py
+++ tasks/stktask.py
@@ -8,7 +8,7 @@ import sys
 
 sys.path.append('../conf/')
 
-app = Celery('tasks',backend='redis://:hellboy@103.235.232.114/0',broker='redis://:hellboy@103.235.232.114/0')
+app = Celery('stktask',backend='redis://:hellboy@103.235.232.114/0',broker='redis://:hellboy@103.235.232.114/0')
 # app.config_from_object('celeryconfig')
 
 class CallbackTask(Task):
@@ -19,11 +19,11 @@ class CallbackTask(Task):
         print "----%s is failure" % task_id
         # pass
 
-@app.task(base=CallbackTask)
+@app.task
 def add(x, y):
     return x + y
 
 
-@app.task(base=CallbackTask)
+@app.task
 def multiply(x,y):
     return x * y
--- worker/stkworker.sh
+++ worker/stkworker.sh
@@ -1,3 +1,3 @@
 cd ../tasks
 celery -A stktask worker -Q priority_high --concurrency=4 -l info -E -n worker1
-#celery -A tasks worker -Q priority_high,priority_low --concurrency=4 -l info -E -n worker2
+#celery -A stktask worker -Q priority_high,priority_low --concurrency=4 -l info -E -n worker2
