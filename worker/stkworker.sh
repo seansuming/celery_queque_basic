a4935d739eafce56faf21792cd37ef993ca756e4
suming <suming@bih.com>
2018-03-21 12:08:04 +0800

    5th

--- worker/stkworker.sh
+++ worker/stkworker.sh
@@ -1,2 +1,3 @@
-celery -A tasks worker -Q priority_high --concurrency=4 -l info -E -n worker1
+cd ../tasks
+celery -A stktask worker -Q priority_high --concurrency=4 -l info -E -n worker1
 #celery -A tasks worker -Q priority_high,priority_low --concurrency=4 -l info -E -n worker2
